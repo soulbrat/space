6320d5853a726667dcb5cb244f1d39261325bb69
soulbrat <soulbrat>
2020-05-22 20:59:05 +0200

    soulbrat

--- src/main/java/com/space/repository/ShipsRepository.java
+++ src/main/java/com/space/repository/ShipsRepository.java
@@ -2,6 +2,7 @@ package com.space.repository;
 
 import com.space.model.Ship;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ShipsRepository extends JpaRepository<Ship, Long> {
 
 }
